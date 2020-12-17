a194256b9ab8a21692a28fe84dd4d49ef4d4a8a8
andreazomoza <aszomoza@gmail.com>
2020-12-17 15:11:10 -0300

    gtag

---
+++ js/analytics/analytics.js
@@ -0,0 +1,6 @@
+$.getScript("https://www.googletagmanager.com/gtag/js?id=G-LDDJJ9M30L", function( data, textStatus, jqxhr ) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-LDDJJ9M30L');
+});
